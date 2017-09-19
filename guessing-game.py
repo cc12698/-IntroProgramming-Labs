8e6e339887f6ea41291baa29dbd08ef2214ce9d1
Chris Crawley <christopher.crawley1@marist.edu>
2017-09-19 14:36:45 -0400

    Usability inprovements to the guessing game, resolves #1 and #2

--- guessing-game.py
+++ guessing-game.py
@@ -1,8 +1,13 @@
-animalAns = {'cow'}
+animalAns = {'cow','Cow', 'COW', 'cOw', 'coW', 'CoW', 'cOW', 'COw'}
+
+leaveGame = {'Quit', 'quit', 'QUIT'}
 
 Thinking = "the computer is thinking on an animal"
 
+leave = "if you would like to exit type quit"
+
 print(Thinking)
+print(leave)
 
 guess = "can you guess the animal? "
 
@@ -14,6 +19,8 @@ while True:
     guess = (input('can you guess the animal? '))
     if guess in animalAns:
         print (congratulations)
+        break
+    elif guess in leaveGame:
         break
     else:
         print (sorry)
