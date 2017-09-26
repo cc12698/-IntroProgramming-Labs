27eacc40e724685e26ef77af758b8b85879183e8
Chris Crawley <christopher.crawley1@marist.edu>
2017-09-26 14:56:53 -0400

    password

--- credentials.py
+++ credentials.py
@@ -18,7 +18,35 @@ def userName():
 userName()
 
 
+def passWord():
 
+    password = input("please enter a password with UpperCase, LowerCase,\nand eight or more characthers: ")
 
+    len(password)
+
+    while True:
+        if len(password) < 8:
+            print("Your password is not long enough")
+
+            password = input('please enter a password: ')
+
+        elif password.isupper():
+            print("you need a lower case letter")
+
+            password = input('please enter a password: ')
+
+        elif password.islower():
+            print("you need upper case letter")
+
+            password = input('please enter a password: ')
+
+        else:
+            print('your password is good')
+
+            break
+
+    
+
+passWord()
 
 
