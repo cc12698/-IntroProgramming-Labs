68c345d8f461cd8bb07913a55d658566855b82c3
Chris Crawley <christopher.crawley1@marist.edu>
2017-10-17 14:12:52 -0400

    it does the command now

---
+++ ArithmeticEngine.py
@@ -0,0 +1,36 @@
+# CMPT 120 - Lab #6
+# YOUR NAME
+# DD-MMM-YYYY
+###
+def showIntro():
+ print("Welcome to the Arithmetic Engine!")
+ print("=================================\n")
+ print("Valid commands are 'add', 'mult', 'sub', 'div', and 'quit'.\n")
+def showOutro():
+ print("\nThank you for using the Arithmetic Engine…")
+ print("\nPlease come back again soon!")
+def doLoop():
+ while True:
+     cmd = input("What computation do you want to perform? ")
+     num1 = int(input("Enter the first number: "))
+     num2 = int(input("Enter the second number: "))
+     if cmd == "add":
+         result = num1 + num2
+         print("The result is " + str(result) + ".\n")
+     elif cmd == "sub":
+         result = num1 - num2
+         print("The result is " + str(result) + ".\n")
+     elif cmd == "mult":
+         result = num1 * num2
+         print("The result is " + str(result) + ".\n")
+     elif cmd == "div":
+         result = num1 // num2
+         print("The result is " + str(result) + ".\n")
+     elif cmd == "quit":
+         break
+ 
+def main():
+ showIntro()
+ doLoop()
+ showOutro()
+main()
